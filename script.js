96eebeb14ea08bc6392d460d2fefd95bc51e89e1
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-22 15:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let input = document.getElementById("evaluatedText");
+let heading = document.getElementById("letterCount");
+heading.innerText = 0;
+input.addEventListener("change", onChange);
+function onChange() {
+	heading.innerText = inpt.value.length;
+}
